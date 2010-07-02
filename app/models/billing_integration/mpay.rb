53bc3c4d03f8a9b9e4b30c2d798c4690766634aa
root <root@current.starseeders.net>
2010-07-02 12:25:39 +0200

    Acutally make the mpay24 request work.

--- app/models/billing_integration/mpay.rb
+++ app/models/billing_integration/mpay.rb
@@ -43,38 +43,64 @@ class BillingIntegration::Mpay < BillingIntegration
 
     # build MDXI XML Block
     xml = Builder::XmlMarkup.new
-    xml.tag! 'order' do
+    xml.instruct! :xml, :version=>"1.0", :encoding=>"UTF-8"
+    xml.tag! 'Order' do
       xml.tag! 'Tid', 'some order identifier'
       xml.tag! 'ShoppingCart' do
         xml.tag! 'Description', 'some description'
+
+        xml.tag! 'Item' do
+          xml.tag! 'Description', 'Some Item'
+          xml.tag! 'Quantity', 4
+          xml.tag! 'ItemPrice', 10.11
+        end
+
+        xml.tag! 'Discount', -20.11
+        xml.tag! 'ShippingCosts', 100.42
+        xml.tag! 'Tax', 22.44
       end
 
-      xml.tag! 'price', 14.5
-      xml.tag! 'BillingAddr', :mode => 'ReadWrite' do
+      xml.tag! 'Price', sprintf("%.2f",14.5)
+      xml.tag! 'BillingAddr', :Mode => 'ReadWrite' do
         xml.tag! 'Name', 'Testname'
         xml.tag! 'Street', 'some street'
+        xml.tag! 'Street2', ''
+        xml.tag! 'Zip', '1040'
         xml.tag! 'City', 'some city'
-#        xml.tag! 'Name', "#{order.ship_address.firstname} #{order.ship_address.lastname}"
-#        xml.tag! 'City', order.ship_address.city
-#        xml.tag! 'Street', order.ship_address.street
-        #TODO: add more address stuff from options hash
+        xml.tag! 'State', ''
+        xml.tag! 'Country', 'Austria'
+        xml.tag! 'Email', ''
       end
 
+      xml.tag! 'ShippingAddr', :Mode => 'ReadOnly' do
+        xml.tag! 'Name', 'Testname'
+        xml.tag! 'Street', 'some street'
+        xml.tag! 'Street2', ''
+        xml.tag! 'Zip', '1040'
+        xml.tag! 'City', 'some city'
+        xml.tag! 'State', ''
+        xml.tag! 'Country', 'Austria'
+        xml.tag! 'Email', ''
+      end
       xml.tag! 'URL' do
-        xml.tag! 'Confirmation', 'some-confirmation-url'
-        xml.tag! 'Notifcation', 'the-callback-url'
+        xml.tag! 'Success', 'some-confirmation-url'
+        xml.tag! 'Confirmation', 'the-callback-url'
       end
     end
 
-    cmd = xml.build!
+    cmd = xml.target!
 
     # create the HTTP request
     # TODO: use the certificate for authentication
     url = URI.parse(gateway_url)
     request = Net::HTTP::Post.new(url.path,{"Content-Type"=>"text/xml"})
     http = Net::HTTP.new(url.host, url.port)
+
+    # verify through SSL
     http.use_ssl = true
-    http.set_debug_output $stderr
+    http.ca_path = "/etc/ssl/certs/"
+    http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+    http.verify_depth = 5
 
     request = Net::HTTP::Post.new(url.request_uri)
     request.set_form_data({
@@ -85,6 +111,19 @@ class BillingIntegration::Mpay < BillingIntegration
 
     response = http.request(request)
 
-    raise response.body.inspect
+    # parse result
+    result = {}
+    response.body.split('&').each do |part|
+      key, value = part.split("=")
+      result[key] = CGI.unescape(value)
+    end
+
+    # if everything did work: return the link url. Otherwise
+    # output an ugly exception (at least we will get notified)
+    if result["STATUS"] == "OK" && result["RETURNCODE"] == "REDIRECT"
+      return result["LOCATION"].chomp
+    else
+      raise response.body.inspect
+    end
   end
 end
