7f0e72cb0b4fe9ea94184cbed70c3185e88703d0
root <root@current.starseeders.net>
2010-07-03 11:39:33 +0200

    actually use spree data and configuration

--- app/models/billing_integration/mpay.rb
+++ app/models/billing_integration/mpay.rb
@@ -1,97 +1,69 @@
 require 'net/https'
 require 'uri'
 
+# TODO: why is only VISA displayed?
+
 # Integrate our payment gateway with spree. This is needed
 # to allow configuration through spree's web interface, etc.
 class BillingIntegration::Mpay < BillingIntegration
 
-  # TODO: add the right preferences, and howto access them from
-  # TODO: the real gateway interface code?
-  preference :merchant_id, :string
-  preference :password, :string
+  preference :production_merchant_id, :string
   preference :test_merchant_id, :string
-  preference :test_merchant_password, :string
 
   def provider_class
     ActiveMerchant::Billing::MpayGateway
   end
 
-  def self.test_mode
-    true
+  def self.current
+    BillingIntegration::Mpay.first(
+                  :conditions => {
+                        :environment => RAILS_ENV,
+                        :active => true
+                  })
   end
 
   TEST_REDIRECT_URL = 'https://test.mPAY24.com/app/bin/etpv5'
   PRODUCTION_REDIRECT_URL = 'https://www.mpay24.com/app/bin/etpv5'
 
-  # TODO this should be preferences
-  #self.production_merchant_id = '72035'
-  #self.test_merchant_id = '92035'
-  
-  def self.gateway_url
-    test_mode == true ? TEST_REDIRECT_URL : PRODUCTION_REDIRECT_URL
+  def gateway_url
+    prefers_test_mode? ? TEST_REDIRECT_URL : PRODUCTION_REDIRECT_URL
+  end
+
+  def merchant_id
+    prefers_test_mode? ? preferred_test_merchant_id : preferred_production_merchant_id
   end
 
   # generate the iframe URL
-  def self.generate_url(order)
-    # TODO: where to get options?
+  def generate_url(order)
 
-    # TODO: howto use the options infrastructure
-    merchant_id = '92035'
-    operation = 'SELECTPAYMENT'
+    cmd = generate_mdxi(order)
 
-    # TODO: merge options
+    # send the HTTP request
+    response = send_request(merchant_id, cmd)
+    result = parse_result(response)
 
-    # build MDXI XML Block
-    xml = Builder::XmlMarkup.new
-    xml.instruct! :xml, :version=>"1.0", :encoding=>"UTF-8"
-    xml.tag! 'Order' do
-      xml.tag! 'Tid', 'some order identifier'
-      xml.tag! 'ShoppingCart' do
-        xml.tag! 'Description', 'some description'
+    # if everything did work out: return the link url. Otherwise
+    # output an ugly exception (at least we will get notified)
+    if result["STATUS"] == "OK" && result["RETURNCODE"] == "REDIRECT"
+      return result["LOCATION"].chomp
+    else
+      raise response.body.inspect
+    end
+  end
 
-        xml.tag! 'Item' do
-          xml.tag! 'Description', 'Some Item'
-          xml.tag! 'Quantity', 4
-          xml.tag! 'ItemPrice', 10.11
-        end
+  private
 
-        xml.tag! 'Discount', -20.11
-        xml.tag! 'ShippingCosts', 100.42
-        xml.tag! 'Tax', 22.44
-      end
-
-      xml.tag! 'Price', sprintf("%.2f",14.5)
-      xml.tag! 'BillingAddr', :Mode => 'ReadWrite' do
-        xml.tag! 'Name', 'Testname'
-        xml.tag! 'Street', 'some street'
-        xml.tag! 'Street2', ''
-        xml.tag! 'Zip', '1040'
-        xml.tag! 'City', 'some city'
-        xml.tag! 'State', ''
-        xml.tag! 'Country', 'Austria'
-        xml.tag! 'Email', ''
-      end
-
-      xml.tag! 'ShippingAddr', :Mode => 'ReadOnly' do
-        xml.tag! 'Name', 'Testname'
-        xml.tag! 'Street', 'some street'
-        xml.tag! 'Street2', ''
-        xml.tag! 'Zip', '1040'
-        xml.tag! 'City', 'some city'
-        xml.tag! 'State', ''
-        xml.tag! 'Country', 'Austria'
-        xml.tag! 'Email', ''
-      end
-      xml.tag! 'URL' do
-        xml.tag! 'Success', 'some-confirmation-url'
-        xml.tag! 'Confirmation', 'the-callback-url'
-      end
+  def parse_result(response)
+    result = {}
+    response.body.split('&').each do |part|
+      key, value = part.split("=")
+      result[key] = CGI.unescape(value)
     end
 
-    cmd = xml.target!
+    result
+  end
 
-    # create the HTTP request
-    # TODO: use the certificate for authentication
+  def send_request(merchant_id, cmd)
     url = URI.parse(gateway_url)
     request = Net::HTTP::Post.new(url.path,{"Content-Type"=>"text/xml"})
     http = Net::HTTP.new(url.host, url.port)
@@ -104,26 +76,73 @@ class BillingIntegration::Mpay < BillingIntegration
 
     request = Net::HTTP::Post.new(url.request_uri)
     request.set_form_data({
-                  'OPERATION' => operation,
+                  'OPERATION' => 'SELECTPAYMENT',
                   'MERCHANTID' => merchant_id,
                   'MDXI' => cmd
     })
 
-    response = http.request(request)
+    http.request(request)
+  end
 
-    # parse result
-    result = {}
-    response.body.split('&').each do |part|
-      key, value = part.split("=")
-      result[key] = CGI.unescape(value)
+  def generate_mdxi(order)
+    xml = Builder::XmlMarkup.new
+    xml.instruct! :xml, :version=>"1.0", :encoding=>"UTF-8"
+    xml.tag! 'Order' do
+      xml.tag! 'Tid', order.id
+      xml.tag! 'ShoppingCart' do
+        xml.tag! 'Description', order.number
+
+        order.line_items.each do |li|
+          xml.tag! 'Item' do
+            xml.tag! 'Description', li.variant.product.name
+            xml.tag! 'Quantity', li.quantity
+            xml.tag! 'ItemPrice', sprintf("%.2f", li.price)
+          end
+        end
+
+        # TODO: why is this 0
+        taxes = order.tax_charges.sum(:amount)
+        xml.tag! 'Tax', sprintf("%.2f", taxes)
+
+        # TODO why is this 0
+        discounts = order.coupon_credits.sum(:amount)
+        xml.tag! 'Discount', sprintf("%.2f", discounts)
+
+        # TODO why is this 0 (amount -> nil)
+        shipment_costs = order.shipping_charges.sum(:amount)
+        xml.tag! 'ShippingCosts', sprintf("%.2f", shipment_costs)
+      end
+
+      xml.tag! 'Price', sprintf("%.2f", order.total)
+
+      xml.tag! 'BillingAddr', :Mode => 'ReadWrite' do
+        xml.tag! 'Name', "#{order.ship_address.firstname} #{order.ship_address.lastname}"
+        xml.tag! 'Street', order.bill_address.address1
+        xml.tag! 'Street2', order.bill_address.address2
+        xml.tag! 'Zip', order.bill_address.zipcode
+        xml.tag! 'City', order.bill_address.city
+        xml.tag! 'State', order.bill_address.state_name
+        xml.tag! 'Country', order.bill_address.country.name
+        xml.tag! 'Email', order.email
+      end
+
+      xml.tag! 'ShippingAddr', :Mode => 'ReadOnly' do
+        xml.tag! 'Name', "#{order.ship_address.firstname} #{order.ship_address.lastname}"
+        xml.tag! 'Street', order.ship_address.address1
+        xml.tag! 'Street2', order.ship_address.address2
+        xml.tag! 'Zip', order.ship_address.zipcode
+        xml.tag! 'City', order.ship_address.city
+        xml.tag! 'State', order.ship_address.state_name
+        xml.tag! 'Country', order.ship_address.country.name
+        xml.tag! 'Email', order.email
+      end
+      xml.tag! 'URL' do
+        # TODO: write controller and provide 'real' URLs
+        xml.tag! 'Success', 'some-confirmation-url'
+        xml.tag! 'Confirmation', 'the-callback-url'
+      end
     end
 
-    # if everything did work: return the link url. Otherwise
-    # output an ugly exception (at least we will get notified)
-    if result["STATUS"] == "OK" && result["RETURNCODE"] == "REDIRECT"
-      return result["LOCATION"].chomp
-    else
-      raise response.body.inspect
-    end
+    xml.target!
   end
 end
