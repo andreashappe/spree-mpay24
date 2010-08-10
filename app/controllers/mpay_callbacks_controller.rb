b98e1bf84f9e7f3ebc060985277855317be27ade
root <root@current.starseeders.net>
2010-08-10 22:48:58 +0200

    automatically redirect from the success-page to the startpage

--- app/controllers/mpay_callbacks_controller.rb
+++ app/controllers/mpay_callbacks_controller.rb
@@ -8,6 +8,7 @@ class MpayCallbacksController < Spree::BaseController
     flash[:commerce_tracking] = "Track Me in GA"
 
     # TODO: this should not be rendered into the IFRAME but in the surrounding page
-    redirect_to order_url(@order, {:checkout_complete => true, :order_token => @order.token})
+    # redirect_to order_url(@order, {:checkout_complete => true, :order_token => @order.token})
+    render :partial => "shared/mpay_success", :locals => { :final_url => order_url(@order, {:checkout_complete => true, :order_token => @order.token})}
   end
 end
