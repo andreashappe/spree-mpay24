a020ba01b514ed7760f431d55e697be053044257
Thomas Winkler <tom@tomsimac.local>
2010-10-10 12:13:15 +0200

    add helper for payment method

---
+++ app/helpers/mpay_helper.rb
@@ -0,0 +1,20 @@
+module MpayHelper
+  
+  def humanize_p_type(p_type)
+    case p_type
+    when "CC"
+      t('CC')
+    else
+      t('Uknown')
+    end
+  end
+  
+  def payment_method(order)
+    return "" unless order
+    return "" if order.checkout.payments.empty?
+    return "" if order.checkout.payment.source.nil?
+    return "" unless order.checkout.payment.source.is_a?(MPaySource)
+    
+    "#{humanize_p_type(order.checkout.payment.source.p_type)}: #{order.checkout.payment.source.brand}"
+  end
+end
