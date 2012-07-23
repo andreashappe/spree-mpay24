1665d480502e1e18e8269661b631dc59f4476a51
Andreas Happe <andreashappe@snikt.net>
2012-07-23 19:01:18 +0200

    use spree test helpers for controller tests

--- spec/controllers/mpay_callbacks_controller_test.rb
+++ spec/controllers/mpay_callbacks_controller_test.rb
@@ -2,8 +2,26 @@
 
 require 'spec_helper'
 
-describe MpayCallbacksController, "some callback thing" do
-  it "should be able to display a status message" do
-    get :index
+describe MpayCallbacksController, "the callback return-screen of the shop" do
+
+  let(:user) { create(:user) }
+  let(:order) { user.orders.create }
+
+  it "should work if the user was logged in" do
+    controller.stub :current_user => user
+    Spree::BillingIntegration::Mpay.current.stub :find_order => order
+
+    spree_get :index, {"TID"=>"assdf_#{order.number}", "LANGUAGE"=>"DE", "USER_FIELD"=>"", "BRAND"=>"VISA"}
+
+    response.should render_template("spree/shared/mpay_success")
+  end
+
+  it "should work if the user isn't logged in" do
+    controller.stub :current_user => nil
+    Spree::BillingIntegration::Mpay.current.stub :find_order => order
+
+    spree_get :index, {"TID"=>"assdf_#{order.number}", "LANGUAGE"=>"DE", "USER_FIELD"=>"", "BRAND"=>"VISA"}
+
+    response.should render_template("spree/shared/mpay_success")
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,6 +12,7 @@ Dir[File.join(File.dirname(__FILE__), 'support/**/*.rb')].each { |f| require f }
 
 # Requires factories defined in spree_core
 require 'spree/core/testing_support/factories'
+require 'spree/core/testing_support/env'
 require 'spree/core/url_helpers'
 
 RSpec.configure do |config|
@@ -23,7 +24,9 @@ RSpec.configure do |config|
   #
   # visit spree.admin_path
   # current_path.should eql(spree.products_path)
+  config.include FactoryGirl::Syntax::Methods
   config.include Spree::Core::UrlHelpers
+  config.include Spree::Core::TestingSupport::ControllerRequests, :type => :controller
 
   # == Mock Framework
   #
