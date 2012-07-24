91afb7b190de044b6ccb9f2b7b0863cd08bf00d4
Andreas Happe <andreashappe@snikt.net>
2012-07-24 18:34:04 +0200

    work on spec test cases a bit

--- spec/controllers/mpay_callbacks_controller_test.rb
+++ spec/controllers/mpay_callbacks_controller_test.rb
@@ -1,5 +1,3 @@
-#require File.dirname(__FILE__) + '/../spec_helper'
-
 require 'spec_helper'
 
 describe MpayCallbacksController, "the callback return-screen of the shop" do
@@ -10,18 +8,30 @@ describe MpayCallbacksController, "the callback return-screen of the shop" do
   it "should work if the user was logged in" do
     controller.stub :current_user => user
     Spree::BillingIntegration::Mpay.current.stub :find_order => order
+    order.stub :token => 1
 
-    spree_get :index, {"TID"=>"assdf_#{order.number}", "LANGUAGE"=>"DE", "USER_FIELD"=>"", "BRAND"=>"VISA"}
+    spree_get :index, {"TID"=>"assdf_#{order.id}", "LANGUAGE"=>"DE", "USER_FIELD"=>"", "BRAND"=>"VISA"}
 
-    response.should render_template("spree/shared/mpay_success")
+    response.should render_template("spree/shared/_mpay_success")
   end
 
   it "should work if the user isn't logged in" do
     controller.stub :current_user => nil
     Spree::BillingIntegration::Mpay.current.stub :find_order => order
+    order.stub :token => 1
 
-    spree_get :index, {"TID"=>"assdf_#{order.number}", "LANGUAGE"=>"DE", "USER_FIELD"=>"", "BRAND"=>"VISA"}
+    spree_get :index, {"TID"=>"assdf_#{order.id}", "LANGUAGE"=>"DE", "USER_FIELD"=>"", "BRAND"=>"VISA"}
 
-    response.should render_template("spree/shared/mpay_success")
+    response.should render_template("spree/shared/_mpay_success")
+  end
+
+  it "output an error if an invalid TID was given" do
+    controller.stub :current_user => nil
+    Spree::BillingIntegration::Mpay.current.stub :find_order => order
+    order.stub :token => 1
+
+    spree_get :index, {"TID"=>"assdf_#{order.id}_xxx", "LANGUAGE"=>"DE", "USER_FIELD"=>"", "BRAND"=>"VISA"}
+
+    response.should_not render_template("spree/shared/_mpay_success")
   end
 end
--- spec/controllers/mpay_confirmations_controller_test.rb
+++ spec/controllers/mpay_confirmations_controller_test.rb
@@ -1,17 +1,17 @@
-#require File.dirname(__FILE__) + '/../spec_helper'
-
 require 'spec_helper'
 
 describe MpayConfirmationController, "receive payment notifications" do
 
   let(:user) { create(:user) }
-  let(:order) { user.orders.create }
 
-  it "should be able to do auto-clearing" do
+  it "should be able to do auto-clearing and complete a valid order" do
+
+    order = user.orders.create
 
     Spree::BillingIntegration::Mpay.current.stub :find_order => order
+    order.stub :state => 'payment'
 
-    options = {"OPERATION"=>"CONFIRMATION", "TID"=>"assdf_#{order.id}", "STATUS"=>"RESERVED", "PRICE"=>"2529", "CURRENCY"=>"EUR", "P_TYPE"=>"CC", "BRAND"=>"VISA", "MPAYTID"=>"1385651", "USER_FIELD"=>"", "ORDERDESC"=>order.number, "CUSTOMER"=>"Andreas Happe", "CUSTOMER_EMAIL"=>order.user.email, "LANGUAGE"=>"DE", "CUSTOMER_ID"=>"", "PROFILE_STATUS"=>"IGNORED", "FILTER_STATUS"=>"", "APPR_CODE"=>"-test-"}
+    options = {"OPERATION"=>"CONFIRMATION", "TID"=>"assdf_#{order.id}", "STATUS"=>"RESERVED", "PRICE"=>"#{(order.total.to_f*100).to_i}", "CURRENCY"=>"EUR", "P_TYPE"=>"CC", "BRAND"=>"VISA", "MPAYTID"=>"1385651", "USER_FIELD"=>"", "ORDERDESC"=>order.number, "CUSTOMER"=>"Andreas Happe", "CUSTOMER_EMAIL"=>order.user.email, "LANGUAGE"=>"DE", "CUSTOMER_ID"=>"", "PROFILE_STATUS"=>"IGNORED", "FILTER_STATUS"=>"", "APPR_CODE"=>"-test-"}
     spree_get :show, options
     response.should be_success
 
@@ -20,6 +20,53 @@ describe MpayConfirmationController, "receive payment notifications" do
     response.should be_success
 
     closed_order = Spree::Order.find(order.id)
-    raise closed_order.state.inspect
+    closed_order.state.should == "complete"
+    #closed_order.payment_state.should == "paid"
+  end
+
+  it "should throw an exception if the order is in the wrong state" do
+
+    order = user.orders.create
+
+    Spree::BillingIntegration::Mpay.current.stub :find_order => order
+    order.stub :state => 'cart'
+
+    options = {"OPERATION"=>"CONFIRMATION", "TID"=>"assdf_#{order.id}", "STATUS"=>"RESERVED", "PRICE"=>"2529", "CURRENCY"=>"EUR", "P_TYPE"=>"CC", "BRAND"=>"VISA", "MPAYTID"=>"1385651", "USER_FIELD"=>"", "ORDERDESC"=>order.number, "CUSTOMER"=>"Andreas Happe", "CUSTOMER_EMAIL"=>order.user.email, "LANGUAGE"=>"DE", "CUSTOMER_ID"=>"", "PROFILE_STATUS"=>"IGNORED", "FILTER_STATUS"=>"", "APPR_CODE"=>"-test-"}
+
+    lambda do
+      spree_get :show, options
+    end.should raise_error
+
+    closed_order = Spree::Order.find(order.id)
+    closed_order.state.should == order.state
+  end
+
+  it "should return an error if there's the wrong currency" do
+
+    order = user.orders.create
+
+    Spree::BillingIntegration::Mpay.current.stub :find_order => order
+    order.stub :state => 'payment'
+
+    options = {"OPERATION"=>"CONFIRMATION", "TID"=>"assdf_#{order.id}", "STATUS"=>"BILLED", "PRICE"=>"2529", "CURRENCY"=>"USD", "P_TYPE"=>"CC", "BRAND"=>"VISA", "MPAYTID"=>"1385651", "USER_FIELD"=>"", "ORDERDESC"=>order.number, "CUSTOMER"=>"Andreas Happe", "CUSTOMER_EMAIL"=>order.user.email, "LANGUAGE"=>"DE", "CUSTOMER_ID"=>"", "PROFILE_STATUS"=>"IGNORED", "FILTER_STATUS"=>"", "APPR_CODE"=>"-test-"}
+
+    lambda do
+      spree_get :show, options
+    end.should raise_error
+  end
+
+  it "should not set the state to complete if the order was underpayed" do
+    order = user.orders.create
+
+    Spree::BillingIntegration::Mpay.current.stub :find_order => order
+    order.stub :state => 'payment'
+
+    options = {"OPERATION"=>"CONFIRMATION", "TID"=>"assdf_#{order.id}", "STATUS"=>"BILLED", "PRICE"=>"5", "CURRENCY"=>"EUR", "P_TYPE"=>"CC", "BRAND"=>"VISA", "MPAYTID"=>"1385651", "USER_FIELD"=>"", "ORDERDESC"=>order.number, "CUSTOMER"=>"Andreas Happe", "CUSTOMER_EMAIL"=>order.user.email, "LANGUAGE"=>"DE", "CUSTOMER_ID"=>"", "PROFILE_STATUS"=>"IGNORED", "FILTER_STATUS"=>"", "APPR_CODE"=>"-test-"}
+
+    spree_get :show, options
+
+    closed_order = Spree::Order.find(order.id)
+    closed_order.payment_state.should == "balance_due"
+    # closed_order.state.should_not == "complete"
   end
 end
