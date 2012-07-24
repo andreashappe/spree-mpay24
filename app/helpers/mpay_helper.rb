29337d5d47de7a15307280acdf44783bbdb86dad
Andreas Happe <andreashappe@snikt.net>
2012-07-24 17:23:21 +0200

    fix: check order's state, also use content_tag..
    ..and cleanup stuff

--- app/helpers/mpay_helper.rb
+++ app/helpers/mpay_helper.rb
@@ -1,24 +1,9 @@
 module MpayHelper
-  def humanize_p_type(p_type)
-    case p_type
-    when "CC"
-      t('CC')
-    else
-      t('Uknown')
-    end
-  end
-  
-  def payment_method(order)
-    return "" unless order
-    return "" if order.checkout.payments.empty?
-    return "" if order.checkout.payment.source.nil?
-    return "" unless order.checkout.payment.source.is_a?(MPaySource)
-    
-    "#{humanize_p_type(order.checkout.payment.source.p_type)}: #{order.checkout.payment.source.brand}"
-  end
-
   def spree_mpay_iframe(order, width=680, height=500)
+
+    raise "order #{order.id} not in state payment but in #{order.state}" if !order.payment?
+
     mpay = Spree::BillingIntegration::Mpay.where(:active => true).where(:environment => Rails.env.to_s).first
-    "<iframe src=\"#{mpay.generate_url(@order)}\" width=#{width}px height=#{height}px></iframe>".html_safe
+    content_tag :iframe, '', :src => mpay.generate_url(@order), :height => "#{height}px", :width => "#{width}px"
   end
 end
