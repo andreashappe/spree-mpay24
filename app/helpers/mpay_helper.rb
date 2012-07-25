1cc02ff2ddc4d3dc9e0d4f325b87645241778f92
Andreas Happe <andreashappe@snikt.net>
2012-07-25 12:38:55 +0200

    restructure helper a bit and fix small typo

--- app/helpers/mpay_helper.rb
+++ app/helpers/mpay_helper.rb
@@ -3,7 +3,13 @@ module MpayHelper
 
     raise "order #{order.id} not in state payment but in #{order.state}" if !order.payment?
 
-    mpay = Spree::BillingIntegration::Mpay.where(:active => true).where(:environment => Rails.env.to_s).first
-    content_tag :iframe, '', :src => mpay.generate_url(@order), :height => "#{height}px", :width => "#{width}px"
+    mpay = retreive_mpay_object
+    content_tag :iframe, '', :src => mpay.generate_url(order), :height => "#{height}px", :width => "#{width}px"
+  end
+
+  private
+
+  def retreive_mpay_object
+    Spree::BillingIntegration::Mpay.where(:active => true).where(:environment => Rails.env.to_s).first
   end
 end
